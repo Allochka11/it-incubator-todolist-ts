879aee3283078dc61e4a672822454657c6a2f564
Allochka11 <allavelychko11@gmail.com>
2023-06-09 15:30:24 +0300

    lesson 16 adding routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,8 @@ import React, {useEffect} from 'react'
 import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import {Menu} from '@mui/icons-material';
 import {setTodolistsTC} from '../features/TodolistList/todolists-reducer'
 import {useAppDispatch, useAppSelector} from './store';
 import {TaskType} from '../api/todolists-api'
@@ -14,6 +11,10 @@ import TodolistList from '../features/TodolistList/TodolistList';
 import LinearProgress from "@mui/material/LinearProgress";
 import ErrorSnackbar from "../components/ErrorSnackbar/ErrorSnackbar";
 import {RequestStatusType} from "./app-reducer";
+import {Login} from "../features/Login/Login";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Error} from "../features/Error/Error";
+import {Avatar} from "@mui/material";
 
 export type TasksStateType = {
     [key: string]: Array<TaskType>
@@ -24,28 +25,47 @@ function App() {
 
     useEffect(() => {
         dispatch(setTodolistsTC())
-    }, [])
+    }, []);
+
 
     const status = useAppSelector<RequestStatusType>(state => state.app.status)
 
     return (
         <div className="App">
-            <AppBar position="static">
+            <AppBar position="static" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
                 <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
+                    {/*<IconButton edge="start" color="inherit" aria-label="menu">*/}
+                    {/*    <Menu/>*/}
+                    {/*</IconButton>*/}
+
                     <Button color="inherit">Login</Button>
+                    <Button color="inherit">Logout</Button>
+
                 </Toolbar>
+                <Avatar
+                    alt="Remy Sharp"
+                    src=""
+                    sx={{ width: 56, height: 56 }}
+                    style={{ margin: '4px 10px 0 0px'}}
+                />
+
             </AppBar>
             {status === 'loading' && <LinearProgress/>}
             <Container fixed>
-                <TodolistList/>
+
+                    <Routes>
+                        <Route path={'/'} element={ <TodolistList/>}/>
+                        <Route path={'/login'} element={ <Login/>}/>
+                        <Route path={'/404'} element={ <Error/>}/>
+                        <Route path="/login/*" element={<Navigate to="/login" replace />} />
+                        <Route path="*" element={<Navigate to="/404"  />} />
+                    </Routes>
+
+
+                {/*<Error/>*/}
             </Container>
             <ErrorSnackbar/>
+
         </div>
     );
 }
---
+++ src/features/Error/Error.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export const Error = () => {
+    return <Grid container justifyContent={'center'}>
+        <Grid item justifyContent={'center'}>
+          <h1>404 Page not found </h1>
+        </Grid>
+    </Grid>
+}
