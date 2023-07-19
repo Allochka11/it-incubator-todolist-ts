b5a99c2b79d7d5430d37e3280e89e53e06238e64
Allochka11 <allavelychko11@gmail.com>
2023-07-19 17:12:57 +0300

    lesson 17 added app.selectors.ts

---
+++ src/app/app.selector.ts
@@ -0,0 +1,11 @@
+import { AppRootStateType } from "app/store";
+import { RequestStatusType } from "app/app-reducer";
+import { TodolistDomainType } from "features/TodolistsList/todolists-reducer";
+import { TasksStateType } from "features/TodolistsList/tasks-reducer";
+
+export const statusSelector = (state: AppRootStateType): RequestStatusType => state.app.status;
+export const isInitializedSelector = (state: AppRootStateType): boolean => state.app.isInitialized;
+export const isLoggedInSelector = (state: AppRootStateType): boolean => state.auth.isLoggedIn;
+export const errorSelector = (state: AppRootStateType): string | null => state.app.error;
+export const todolistsSelector = (state: AppRootStateType): TodolistDomainType[] => state.todolists;
+export const tasksSelector = (state: AppRootStateType): TasksStateType => state.tasks;
