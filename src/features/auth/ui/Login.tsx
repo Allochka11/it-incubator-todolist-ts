0f841c7e92ad921d5f6533837976fa39f5f1d47a
Allochka11 <allavelychko11@gmail.com>
2023-10-16 12:52:36 +0300

    added useLogin

--- src/features/auth/ui/Login.tsx
+++ src/features/auth/ui/Login.tsx
@@ -1,53 +1,10 @@
 import React from "react";
-import { FormikHelpers, useFormik } from "formik";
-import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { useAppDispatch } from "common/hooks/useAppDispatch";
 import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from "@mui/material";
-import { isLoggedInSelector } from "app/app.selector";
-import { authThunks } from "features/auth/model/auth-reducer";
-import { LoginParamsType } from "features/auth/api/auth.api";
-import { BaseResponseType } from "common/api";
-
-type FormikErrorType = Partial<Omit<LoginParamsType, "captcha">>;
+import { useLogin } from "features/auth/lib/useLogin";
 
 export const Login = () => {
-  const dispatch = useAppDispatch();
-  const isLoggedIn = useSelector(isLoggedInSelector);
-
-  const formik = useFormik({
-    validate: (values) => {
-      const errors: FormikErrorType = {};
-
-      if (!values.email) {
-        errors.email = "Required";
-      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = "Invalid email address";
-      }
-
-      if (!values.password) {
-        errors.password = "Required password";
-      } else if (values.password.length < 8) {
-        errors.password = "Invalid password";
-      }
-      return errors;
-    },
-    initialValues: {
-      email: "",
-      password: "",
-      rememberMe: false,
-    },
-    onSubmit: (values, formikHelpers: FormikHelpers<LoginParamsType>) => {
-      dispatch(authThunks.login(values))
-        .unwrap()
-        .then((res) => {})
-        .catch((err: BaseResponseType) => {
-          err.fieldsErrors?.forEach((e) => {
-            formikHelpers.setFieldError(e.field, e.error);
-          });
-        });
-    },
-  });
+  const { formik, isLoggedIn } = useLogin();
 
   if (isLoggedIn) {
     return <Navigate to={"/"} />;
